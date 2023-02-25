4a21cbce0d1bad7134cd059907e5b71bd96cb10f
Mizuma Takuya <rp008077@i.softbank.jp>
2023-02-26 02:43:39 +0900

    Add ai option

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 #! /usr/bin/env node
 
-import { program } from "commander";
-import { generate } from "./src/commands/generate/index.js";
+import { program } from 'commander';
+import { generate } from './src/commands/generate/index.js';
 
 program
-  .command("generate")
+  .command('generate')
   .description(
-    "otdgen generates JavaScript/TypeScript test data from the OpenApi schema file(yaml) to the folder specified by the argument."
+    'otdgen generates JavaScript/TypeScript test data from the OpenApi schema file(yaml) to the folder specified by the argument.'
   )
-  .requiredOption("-i, --input <path>")
-  .requiredOption("-o, --output <path>")
-  .option("-n, --number-of-array-data <number>")
-  .option("-ext, --extension <output file extension>")
-  .option("-ignore, --ignore <ignore schema list separated by commas>")
-  .option("-s, --stateless <true | false>")
-  .option("-es, --example-suffix <true | false>")
+  .requiredOption('-i, --input <path>')
+  .requiredOption('-o, --output <path>')
+  .option('-ai, --ai-mode <true | false>')
+  .option('-n, --number-of-array-data <number>')
+  .option('-ext, --extension <output file extension>')
+  .option('-ignore, --ignore <ignore schema list separated by commas>')
+  .option('-s, --stateless <true | false>')
+  .option('-es, --example-suffix <true | false>')
   .action(generate);
 
 program.parse(process.argv);
